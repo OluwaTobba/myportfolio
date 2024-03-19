f9eb390b1de9d129b3c0de8faa31d5dc4ac435eb
OluwaTobba <onaopemipomichael20@gmail.com>
2024-03-19 14:51:28 +0100

    update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -6,7 +6,22 @@ const sendEmail = (e) => {
     e.preventDefault();
 
     // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_3853g2u','','','')
+    emailjs.sendForm('service_3853g2u','template_s833dps','#contact-form','CvA6c6t7SOETGtE8W')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (Service Error) ❌'
+        });
 }
 
 contactForm.addEventListener('submit', sendEmail);
